877b12646c5bf96eb55eb4b5ff920ff75bec2062
Dale Thatcher <dale.thatcher@gmail.com>
2020-12-28 17:16:00 +0000

    Add acknowledgements for used images.

--- src/index.tsx
+++ src/index.tsx
@@ -5,10 +5,15 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App/>
+        <footer className="Index-footer">
+            <a href={process.env.PUBLIC_URL + "/acknowledgements.txt"}
+               target="_blank"
+               rel="noreferrer">acknowledgments</a>
+        </footer>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
