dc161059b45c0795c75a86538f442eec4470685f
Dale Thatcher <dale.thatcher@gmail.com>
2020-12-28 08:04:38 +0000

    Add real choices.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,43 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-class App extends React.Component<{}, { coin: String }> {
-    readonly state = {coin: "Press flip to start"};
+interface SpinnerState {
+    pointerIndex: number
+}
 
-    flipCoin(event: React.MouseEvent) {
+const choices = [
+    "send someone back one!",
+    "forward one or move wooden plank",
+    "forward one or move rope bridge",
+    "forward two",
+    "forward three!"
+]
+
+class App extends React.Component<{}, SpinnerState> {
+    readonly state = {pointerIndex: -1}
+
+    spinSpinner(event: React.MouseEvent) {
         event.preventDefault();
-        const heads = Math.trunc(Math.random() * 2.0) < 1;
+        const newPointerIndex = Math.trunc(Math.random() * choices.length);
 
-        this.setState((state, props) => {
-            return {coin: heads ? "heads" : "tails"}
-        }, () => {
-
+        this.setState(() => {
+            return {pointerIndex: newPointerIndex}
         });
+    }
+
+    pointerString() {
+        if (this.state.pointerIndex < 0) {
+            return "press spin to start"
+        } else {
+            return choices[this.state.pointerIndex]
+        }
     }
 
     render() {
         return <div className="App">
             <header className="App-header">
-                <p>{this.state.coin}</p>
-                <button onClick={e => this.flipCoin(e)}>Flip</button>
+                <p>{this.pointerString()}</p>
+                <button onClick={e => this.spinSpinner(e)}>Spin</button>
             </header>
         </div>;
     }
